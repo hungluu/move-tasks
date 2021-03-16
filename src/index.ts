4267865a56654d005b7016b7afb78c1ec3c91b69
Hung Luu <hungluu2106@gmail.com>
2021-03-17 01:37:26 +0700

    Init codebase

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import core from '@actions/core'
+import github from '@actions/github'
+
+async function run (): Promise<void> {
+  const token = core.getInput('ACTION_TOKEN')
+  const projectId = parseInt(core.getInput('PROJECT_ID'))
+
+  const octokit = github.getOctokit(token)
+
+  const project = await octokit.projects.listColumns({
+    project_id: projectId
+  })
+
+  console.log(project.data)
+}
+
+run().catch(error => core.setFailed(error.message))
